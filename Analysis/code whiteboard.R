55956a611e8b14245e9e4816e3e4fa8cad81d12f
noahhaber <noahhaber@gmail.com>
2023-10-16 16:29:23 -0400

    Updating whiteboard file

--- Analysis/code whiteboard.R
+++ Analysis/code whiteboard.R
@@ -10,11 +10,15 @@
   library(devtools)
   library(ggsankey)
   library(plotly)
+  library(viridis)
 }
 
-tar_load("repli_export")
-tar_load("repli_outcomes")
+# tar_make() # run this to pull new targets
 
+tar_load("repli_export")
+tar_load("repli_primary")
+tar_load("repli_outcomes")
+tar_load("orig_statistics_dataset_p1")
 
 # UI and selection options data
 {
@@ -39,15 +43,15 @@ tar_load("repli_outcomes")
                                            select_rr_analytic_sample_stage_selection_ui = c(TRUE,TRUE,FALSE,FALSE,FALSE)){
     
     # Pull data
-    tar_load("repli_export")
+    #tar_load("repli_export")
     tar_load("repli_outcomes")
     #tar_load("repli_primary")
     
     # Temporary for data "cleaning"
-    repli_export$generalizability <- ifelse(repli_export$paper_id %in% c("Br0x", "EQxa", "J4W9", "plLK", "rjb", "zlm2"),
+    repli_outcomes$generalizability <- ifelse(repli_outcomes$paper_id %in% c("Br0x", "EQxa", "J4W9", "plLK", "rjb", "zlm2"),
                                             TRUE,FALSE)
-    repli_export$claim_id_unique <- paste0(repli_export$paper_id,"_",repli_export$claim_id)
-    repli_export$rr_type_internal <- NULL
+    repli_outcomes$claim_id_unique <- paste0(repli_outcomes$paper_id,"_",repli_outcomes$claim_id)
+    repli_outcomes$rr_type_internal <- NULL
     
     select_rr_type_selection <- select_rr_type_set[select_rr_type_selection_ui]
     select_generalizability_selection <- select_generalizability_set[select_generalizability_selection_ui]
@@ -55,37 +59,101 @@ tar_load("repli_outcomes")
     select_rr_analytic_sample_stage_selection <- select_rr_analytic_sample_stage_set[select_rr_analytic_sample_stage_selection_ui]
     
     # Create selected dataset
-    repli_export_selected <- repli_export[repli_export$rr_type %in% select_rr_type_selection &
-                                            repli_export$generalizability %in% select_generalizability_selection &
-                                            repli_export$rr_is_manylabs %in% select_rr_is_manylabs_selection &
-                                            repli_export$rr_analytic_sample_stage %in% select_rr_analytic_sample_stage_selection,]
+    repli_outcomes_selected <- repli_outcomes[repli_outcomes$rr_type %in% select_rr_type_selection &
+                                            repli_outcomes$generalizability %in% select_generalizability_selection &
+                                            repli_outcomes$rr_is_manylabs %in% select_rr_is_manylabs_selection &
+                                            repli_outcomes$rr_analytic_sample_stage %in% select_rr_analytic_sample_stage_selection,]
     # Generate selection weights and set indexes (for combining)
-    repli_export_selected <- repli_export_selected %>% 
+    repli_outcomes_selected <- repli_outcomes_selected %>% 
       group_by(paper_id) %>% 
       mutate(weight_by_paper_id = 1/n())
-    repli_export_selected <- repli_export_selected %>% 
+    repli_outcomes_selected <- repli_outcomes_selected %>% 
       group_by(claim_id_unique) %>% 
       mutate(weight_by_claim_id = 1/n())
   }
 }
 
-# Compare power Tillburg vs success rate
+# Aesthetic functions
 {
-  df.chart <- generate_selected_repli_data()
+  GeomSplitViolin <- ggproto("GeomSplitViolin", GeomViolin, 
+                             draw_group = function(self, data, ..., draw_quantiles = NULL) {
+                               data <- transform(data, xminv = x - violinwidth * (x - xmin), xmaxv = x + violinwidth * (xmax - x))
+                               grp <- data[1, "group"]
+                               newdata <- plyr::arrange(transform(data, x = if (grp %% 2 == 1) xminv else xmaxv), if (grp %% 2 == 1) y else -y)
+                               newdata <- rbind(newdata[1, ], newdata, newdata[nrow(newdata), ], newdata[1, ])
+                               newdata[c(1, nrow(newdata) - 1, nrow(newdata)), "x"] <- round(newdata[1, "x"])
+                               
+                               if (length(draw_quantiles) > 0 & !scales::zero_range(range(data$y))) {
+                                 stopifnot(all(draw_quantiles >= 0), all(draw_quantiles <=
+                                                                           1))
+                                 quantiles <- ggplot2:::create_quantile_segment_frame(data, draw_quantiles)
+                                 aesthetics <- data[rep(1, nrow(quantiles)), setdiff(names(data), c("x", "y")), drop = FALSE]
+                                 aesthetics$alpha <- rep(1, nrow(quantiles))
+                                 both <- cbind(quantiles, aesthetics)
+                                 quantile_grob <- GeomPath$draw_panel(both, ...)
+                                 ggplot2:::ggname("geom_split_violin", grid::grobTree(GeomPolygon$draw_panel(newdata, ...), quantile_grob))
+                               }
+                               else {
+                                 ggplot2:::ggname("geom_split_violin", GeomPolygon$draw_panel(newdata, ...))
+                               }
+                             })
   
+  geom_split_violin <- function(mapping = NULL, data = NULL, stat = "ydensity", position = "identity", ..., 
+                                draw_quantiles = NULL, trim = TRUE, scale = "area", na.rm = FALSE, 
+                                show.legend = NA, inherit.aes = TRUE) {
+    layer(data = data, mapping = mapping, stat = stat, geom = GeomSplitViolin, 
+          position = position, show.legend = show.legend, inherit.aes = inherit.aes, 
+          params = list(trim = trim, scale = scale, draw_quantiles = draw_quantiles, na.rm = na.rm, ...))
+  }
 }
 
-# Basic replication stats
+# # Compare power Tillburg vs success rate
+# {
+#   df.chart <- generate_selected_repli_data()
+#   
+# }
+
+# Main 3 parter
 {
-  df.chart <- generate_selected_repli_data()
+  # Generate dataset
+    df.chart <- generate_selected_repli_data()
+    
+    # TEMPORARY FOR MADE UP DATA
+    df.chart$rr_pearsons_r_value_reference <- df.chart$rr_pearsons_r_value*1.1
+    
+    # Gather up new vs originals
+      # Pearsons
+        df.chart.pearsons <- df.chart[c("rr_pearsons_r_value","rr_effect_size_value_reference")]
+        df.chart.pearsons$stat_type <- "Pearson's R"
+        colnames(df.chart.pearsons) <- c("Replication","Original","stat_type")
+        
+      # All others
+        df.chart.others <- df.chart[c("rr_statistic_value_reported","rr_effect_size_value_reference","rr_effect_size_type_reported")]
+        colnames(df.chart.others) <- c("Replication","Original","stat_type")
+        types_to_keep <- c("ser_method")
+        df.chart.others <- df.chart.others[df.chart.others$stat_type %in% types_to_keep,]
+        
+      # Combine
+        df.chart <- rbind(df.chart.pearsons,df.chart.others)
+        df.chart <- df.chart %>% pivot_longer(!"stat_type", names_to = "comparison", values_to = "ES_value")
+        df.chart <- na.omit(df.chart)
   
-  ggplot(data=repli_export,aes(y=)) +
-    geom_histogram()+
-    coord_flip()+
+  ggplot(data=df.chart,aes(y=ES_value,x=stat_type,fill=comparison)) +
+    geom_split_violin()+
+    #geom_histogram(fill="#2B2484",alpha=.4)+
+    #geom_density(fill="#2B2484",alpha=.8)+
+    #scale_x_continuous(expand=c(0,0))+
+    #scale_y_continuous(expand=c(0,0))+
     theme_minimal()+
-    scale_y_continuous(limits=c(-50,50))
+    theme(
+      legend.position = "none",
+      panel.grid = element_blank(),
+
+      axis.line = element_line(color="#393939")
+    )
+    #scale_fill_viridis()+
+    #coord_flip()
   
-  target
 }
 
 
@@ -95,7 +163,7 @@ tar_load("repli_outcomes")
 
 # Play area 
 if(FALSE){
-  # Testing for full hierarchy
+  # Main 3
   {
     df.hierarchy <- repli_export %>% group_by(paper_id,claim_id,rr_analytic_sample_stage,rr_is_manylabs, .drop = FALSE) %>% count()
     dup_paper_ids <- df.hierarchy$paper_id[df.hierarchy$n==2]
@@ -103,42 +171,6 @@ if(FALSE){
     
     test <- repli_export %>% group_by(paper_id,claim_id,rr_is_manylabs, .drop = FALSE) %>% count()
     
-  }
-  # Generate Sankey style chart
-  {
-    # Generate hierarchical structure
-    repli_hierarchy_order <- c("paper_id","claim_id","rr_is_manylabs","rr_analytic_sample_stage","rr_id")
-    repli_hierarchy_order_label <- c("Unique paper","Unique claim","ManyLabs participation","Sample stage\n(for case of multiple stepped RRs)","Other")
-    df.hier <- repli_export[,c()]
-    df.hier[[paste0("hier_level_",1)]] <- repli_export[[repli_hierarchy_order[1]]]
-    for (i in 2:length(repli_hierarchy_order)){
-      df.hier[[paste0("hier_level_",i)]] <- paste(df.hier[[paste0("hier_level_",i-1)]] ,repli_export[[repli_hierarchy_order[i]]],sep="_")
-    }
-    # Generate alluvial structure
-    df.alluvial <- df.hier %>% 
-      group_by(across(all_of(colnames(df.hier)))) %>% 
-      #group_by(paper_id,claim_id_unique_1,claim_id_unique_2, .drop = FALSE) %>% 
-      count()
-    
-    df.alluvial <- df.alluvial[order(df.alluvial$hier_level_1),]
-    
-    #
-    df.alluvial <- df.alluvial[df.alluvial$hier_level_1 %in% unique(df.alluvial$hier_level_1)[1:10],]
-    
-    ggplot(df.alluvial,
-           aes(y = n, axis1 = hier_level_1, axis2 = hier_level_2,axis3 = hier_level_3,axis4 = hier_level_4,axis5 = hier_level_5,
-               fill=hier_level_1)) +
-      geom_alluvium() + 
-      geom_stratum(size=0.1)+
-      theme_minimal()+
-      theme(
-        legend.position = "none",
-        panel.grid = element_blank(),
-        axis.title.y = element_blank(),
-        axis.text.y = element_blank()
-      )+
-      scale_x_continuous(breaks=1:length(repli_hierarchy_order),labels=repli_hierarchy_order_label)
-    #geom_alluvium(aes(fill = Admit), width = 1/12)
   }
   
   
@@ -165,7 +197,7 @@ if(FALSE){
             , axis.ticks = element_blank()  
             , panel.grid = element_blank()) +
       labs(fill = 'Nodes')
-    # pl <- pl + scale_fill_viridis_d(option = "inferno")
+     pl <- pl + scale_fill_viridis_d(option = "inferno")
     # pl <- pl + labs(title = "Sankey diagram using ggplot")
     # pl <- pl + labs(subtitle = "using  David Sjoberg's ggsankey package")
     pl
