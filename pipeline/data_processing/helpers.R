dab3e6eacc0fd20cfd2d52c2f709c277f4c9bd9f
Theresa Wisneskie Stankov <82187440+twisneskie@users.noreply.github.com>
2023-12-12 12:01:30 -0500

    Add repro summary variables

--- pipeline/data_processing/helpers.R
+++ pipeline/data_processing/helpers.R
@@ -193,3 +193,11 @@ load_changelog <- function(orig_input_changelog_file,
            across(c(change_to, change_from), ~na_if(., "NC")))
   
 }
+
+# Customized rounding function that pushes 5 up rather than down
+round_off <- function(x, digits = 0) {
+  posneg <- sign(x)
+  z <- trunc(abs(x) * 10 ^ (digits + 1)) / 10
+  z <- posneg * (floor(z + 0.5) / 10 ^ digits)
+  return(z)
+}
