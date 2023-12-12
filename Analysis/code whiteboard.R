f65c245c1925cae2f0019400d84327e3ce6e51b0
noahhaber <noahhaber@gmail.com>
2023-12-12 12:30:48 -0500

    Revert "Merge pull request #3 from CenterForOpenScience/Analysis-updates"
    This reverts commit 17e37961ab36e91bdc474972d99594eb9b7859ea, reversing
    changes made to d25f86dae08088331894088d3c1faaf44ae5074a.

--- Analysis/code whiteboard.R
+++ Analysis/code whiteboard.R
@@ -11,37 +11,16 @@
   library(ggsankey)
   library(plotly)
   library(viridis)
-  
 }
 
 # tar_make() # run this to pull new targets
 
-
-#tar_load("repli_export")
-#tar_load("repli_primary")
+tar_load("repli_export")
+tar_load("repli_primary")
 tar_load("repli_outcomes")
-#tar_load("orig_statistics_dataset_p1")
+tar_load("orig_statistics_dataset_p1")
 
-# #tar_load("repli_export")
-# #tar_load("repli_primary")
-# tar_load("repli_outcomes")
-# #tar_load("orig_statistics_dataset_p1")
-# tar_load("orig_dataset")
-
-
-# Update app data
-if (FALSE){
-  tar_load("repli_outcomes")
-  save(repli_outcomes,file="Analysis/Data exploration app/repli_outcomes.RData")
-
-  tar_load("orig_dataset")
-  save(orig_dataset,file="Analysis/Data exploration app/orig_dataset.RData")
-  
-  tar_load("repro_export")
-
-
-}
-
+save(repli_outcomes,file="Analysis/Data exploration app/repli_outcomes.RData")
 
 # UI and selection options data
 {
@@ -60,7 +39,7 @@ if (FALSE){
 
 # Generate selection data functions
 {
-  df_repli_subsetted <- function(select_rr_type_selection_ui = c(TRUE,FALSE,FALSE),
+  generate_selected_repli_data <- function(select_rr_type_selection_ui = c(TRUE,FALSE,FALSE),
                                            select_generalizability_selection_ui = c(TRUE,FALSE),
                                            select_rr_is_manylabs_selection_ui = c(TRUE,FALSE,FALSE,FALSE),
                                            select_rr_analytic_sample_stage_selection_ui = c(TRUE,TRUE,FALSE,FALSE,FALSE)){
@@ -128,267 +107,110 @@ if (FALSE){
           position = position, show.legend = show.legend, inherit.aes = inherit.aes, 
           params = list(trim = trim, scale = scale, draw_quantiles = draw_quantiles, na.rm = na.rm, ...))
   }
-  # Color palette
-  {
-    palette_weezer_blue <- c("#00a2e7","#dee5cd","#010c09","#083259","#b2915f","#d7b1b7","#00374b","#124e80", "#001212")
-    palette_weezer_pinkerton <- c("#d5bf98","#14140b","#70624b","#8a8d82","#304251","#465656","#945a2d","#708090")
-    palette_weezer_green <- c("#bece30","#020100","#4f6238","#cac986","#981f2c","#c13f33","#461005")
-    palette_weezer_maladroit <- c("#e0dcce","#575b61","#b69e44","#953d31","#e5b066","#343729","#3e3131")
-    palette_weezer_make_believe <- c("#000000","#EAECEB","#C2C2C2","#A0A0A0","#313131")
-    palette_weezer_red <- c("#ED1B34","#8A817C","#141311","#8B8D9C","#332E28")
-    palette_weezer_raditude <- c("#EC2221","#FBFFFB","#FDF600","#CEB181","#4E1110")
-    palette_weezer_everything <- c("#E8A662","#F4F5F1","#463D47","#7F3009","#35180E","F6F3CF")
-    palette_weezer_white <- c("#FDFDFD","#242424","#E3E3E3","#B6B6B6","#EEEDED")
-    palette_weezer_pacific_daydream <- c("#1E3555","#5C6455","#FBE4BC","#1D1F1E","#69797B","#F8E6CF","#F8E6CF")
-    palette_weezer_teal <- c("#1DBBBE","#D6A8CD","#F8F8F8","#182633","#90C5DF")
-    palette_weezer_black <- c("#2D2B2C","#060606","#E9E7E8","#0E0E0E")
-    palette_weezer_ok_human <- c("#B2A17A","#B3B470","#B1A78F","#D1BE8F","#726D5C","#B8B6A6","#5B4F3F")
-    palette_weezer_van_weezer <- c("#B2023E","#E933D3","#770265","#170032","#FDF8FF","#170032","#5329F9","#F3FED5")
-    
-    plaette_score_charts <- c(palette_weezer_blue[1],
-                              palette_weezer_red[1],
-                              palette_weezer_green[1],
-                              palette_weezer_teal[1],
-                              
-                              palette_weezer_pinkerton[1],
-                              palette_weezer_van_weezer[3]
-    )
-  }
 }
+
+# # Compare power Tillburg vs success rate
+# {
+#   df.chart <- generate_selected_repli_data()
+#   
+# }
 
 # Main 3 parter
 {
-  tar_load("repli_outcomes")
-  df.chart <- repli_outcomes
-  #df.chart <- df_repli_subsetted()
+  # Generate dataset
+    df.chart <- generate_selected_repli_data()
+    
+    # TEMPORARY FOR MADE UP DATA
+    df.chart$rr_pearsons_r_value_reference <- df.chart$rr_pearsons_r_value*1.1
+    
+    # Gather up new vs originals
+      # Pearsons
+        df.chart.pearsons <- df.chart[c("rr_pearsons_r_value","rr_effect_size_value_reference")]
+        df.chart.pearsons$stat_type <- "Pearson's R"
+        colnames(df.chart.pearsons) <- c("Replication","Original","stat_type")
+        
+      # All others
+        df.chart.others <- df.chart[c("rr_statistic_value_reported","rr_effect_size_value_reference","rr_effect_size_type_reported")]
+        colnames(df.chart.others) <- c("Replication","Original","stat_type")
+        types_to_keep <- c("ser_method")
+        df.chart.others <- df.chart.others[df.chart.others$stat_type %in% types_to_keep,]
+        
+      # Combine
+        df.chart <- rbind(df.chart.pearsons,df.chart.others)
+        df.chart <- df.chart %>% pivot_longer(!"stat_type", names_to = "comparison", values_to = "ES_value")
+        df.chart <- na.omit(df.chart)
+        df.chart$stat_type <- factor(df.chart$stat_type,
+                                     labels=c("Pearson's R","SER"),
+                                     levels=c("Pearson's R","ser_method"))
+        df.chart$comparison <- factor(df.chart$comparison,
+                                     labels=c("Replication","Original"),
+                                     levels=c("Replication","Original"))
   
-  # Merge in orig data
-  tar_load("orig_dataset")
-  df.chart.orig <- orig_dataset[c("unique_claim_id",
-                                  "original_effect_size_value_reported",
-                                  "original_effect_size_type_reference",
-                                  "original_pearsons_r_numeric")]
-  df.chart <- merge(df.chart,df.chart.orig,by.x="claim_id",by.y="unique_claim_id",all.x=TRUE,all.y=FALSE)
-
-  df.chart$orig_pearsons_r <- as.numeric(df.chart$original_pearsons_r_numeric)
-  df.chart$orig_effect_size_value <- as.numeric(df.chart$original_effect_size_value_reported)
-  
-  # Gather up new vs originals
-  # Pearsons
-  df.chart.pearsons <- df.chart[c("repli_pearsons_r_value","orig_pearsons_r")]
-  df.chart.pearsons$stat_type <- "Pearson's R"
-  colnames(df.chart.pearsons) <- c("Replication","Original","stat_type")
-  
-  # All others
-  df.chart.others <- df.chart[c("repli_effect_size_value","orig_effect_size_value","repli_effect_size_type")]
-  colnames(df.chart.others) <- c("Replication","Original","stat_type")
-  types_to_keep <- c("ser_method")
-  df.chart.others <- df.chart.others[df.chart.others$stat_type %in% types_to_keep,]
-  
-  # Combine
-  df.chart <- rbind(df.chart.pearsons,df.chart.others)
-  df.chart <- df.chart %>% pivot_longer(!"stat_type", names_to = "comparison", values_to = "ES_value")
-  df.chart <- na.omit(df.chart)
-  df.chart$stat_type <- factor(df.chart$stat_type,
-                               labels=c("Pearson's R","SER"),
-                               levels=c("Pearson's R","ser_method"))
-  df.chart$comparison <- factor(df.chart$comparison,
-                                labels=c("Replication","Original"),
-                                levels=c("Replication","Original"))
-  df.chart$jitter_bias <- ifelse(df.chart$comparison=="original",-.2,.2)
-  
-  ggplot(data=df.chart,aes(y=ES_value,x=stat_type,fill=reorder(comparison, desc(comparison)))) +
+  ggplot(data=df.chart,aes(y=ES_value,x=stat_type,fill=comparison)) +
     geom_split_violin()+
-    geom_point(position=position_jitterdodge(),size=.2)+
-    theme_bw()+
-    scale_fill_manual(values=plaette_score_charts)+
-    geom_hline(aes(yintercept =0),linetype=3,color="#454545")+
+    #geom_histogram(fill="#2B2484",alpha=.4)+
+    #geom_density(fill="#2B2484",alpha=.8)+
+    #scale_x_continuous(expand=c(0,0))+
+    #scale_y_continuous(expand=c(0,0))+
+    theme_minimal()+
     theme(
       legend.position = "bottom",
       panel.grid = element_blank(),
-      axis.line = element_line(color="#393939"),
-      legend.title=element_blank()
+      axis.line = element_line(color="#393939")
     )+
-    scale_y_continuous(expand=c(0,0),limits=c(-0,1))+
     xlab("Statistic type")+
     ylab("Effect size value")
+    #scale_fill_viridis()+
+    #coord_flip()
   
 }
 
-  
-   
-# Generate alluvial
-{
-  # Data generation
-  
-  tar_load("repli_outcomes")
-  tar_load("orig_dataset")
-  tar_load("repro_export")
-  
-  df.alluvial <- orig_dataset[c("paper_id","claim_id")]
-  df.alluvial$claim_id <- paste0(df.alluvial$paper_id,"_",df.alluvial$claim_id)
-  
-  repro.alluvial <- repro_export[c("paper_id","claim_id")]
-  repro.alluvial$claim_id <- paste0(repro.alluvial$paper_id,"_",repro.alluvial$claim_id)
-  repro.alluvial$paper_id <- NULL
-  repro.alluvial$reproduction <- TRUE
-  repro.alluvial$reproduction_outcome <- ifelse(repro_export$rr_repro_success_reported=="yes","Reproduced","Not reproduced")
-  
-  repli.alluvial <- repli_outcomes[c("claim_id")]
-  repli.alluvial$replication <- TRUE
-  repli.alluvial$replication_outcome <- ifelse(repli_outcomes$repli_pattern_criteria_met==TRUE,"Replicated","Not replicated")
-  
-  df.alluvial <- merge(df.alluvial,repro.alluvial,by = "claim_id",all.x=TRUE,all.y = FALSE)
-  df.alluvial <- merge(df.alluvial,repli.alluvial,by = "claim_id",all.x=TRUE,all.y = FALSE)
-  
-  
-  df.alluvial <- rbind(repro.alluvial,repli.alluvial)
-  
-  # df.repli.no.hier <- repli_outcomes[c("paper_id","claim_id","is_manylabs","power_for_effect_size")]
-  # df.repli.no.hier$claim_id <- paste0(df.repli.no.hier$paper_id,"_", df.repli.no.hier$claim_id)
-  
-  #df.repli.no.hier <- df.repli.no.hier[df.repli.no.hier$paper_id %in% unique(df.repli.no.hier$paper_id)[60:120],]
-  
-  df.alluvial <- df.alluvial %>%
-    ggsankey::make_long(paper_id,claim_id,type,outcome)
-  
-  ggplot(df.alluvial, aes(x = x, next_x = next_x, node = node, next_node = next_node, fill = factor(node), label = node)) + 
-    ggsankey::geom_alluvial(flow.alpha = 0.75, show.legend = FALSE,space=1) +
-    #ggsankey::geom_sankey()+
-    #geom_sankey_label(size = 3, color = "black", fill= "white", hjust = -0.5)+
-    theme_bw() + 
-    theme(legend.position = "none") +
-    theme(axis.title = element_blank()
-          , axis.text.x = element_blank()
-          , axis.ticks = element_blank()  
-          , panel.grid = element_blank()) +
-    labs(fill = 'Nodes')+
-    coord_flip()
-}
-
-library(ggplot2)
-
-ID <- c(1,1,2,2,3,3,4,4,5,5)
-group <- c(20,20, 50, 50,20, 20, 80, 80, 80, 80)
-condition <- c("med", "placebo","med", "placebo","med", "placebo","med", "placebo","med", "placebo")
-PropYes <- c(0.13, 0.15, 0.25, 0.13, 0.54, 0.34, 0.23, 0.45, 0.142, 0.344)
-exampleData <- data.frame(ID, group, condition, PropYes)
-exampleData <- within(exampleData, {
-  group <- as.factor(group)
-  condition <- as.factor(condition)
-})
-
-pd = position_dodge(width=0)
-ggplot(exampleData, aes(x=factor(condition), y=PropYes, 
-                        color=factor(group), group=factor(ID))) + 
-  geom_point(position=pd) + geom_line(position=pd)
-
-
-ggplot(df.chart, aes(x=comparison, y=ES_value, 
-                        group=factor(group))) + 
-  geom_point(position=pd) + geom_line(position=pd)
-
-ggplot(df.chart, aes(x=comparison, y=ES_value, 
-                     group=factor(group))) + 
-  geom_point() + geom_line()
 
 
 
 
-# Startup and initialization
-{
-  rm(list=ls()) # yes I know this is bad, will get rid of later; just a convenience for now
-  
-  library(targets)
-  library(ggplot2)
-  library(ggalluvial)
-  library(tidyr)
-  library(dplyr)
-  library(devtools)
-  library(ggsankey)
-  library(plotly)
-  library(viridis)
-}
 
-tar_load("repli_outcomes")
-
-bootstrap.clust <- function(data=NA,FUN=NA,clustervar=NA,
-                            alpha=.05,tails="two-tailed",iters=100){
-  # Set up cluster designations
-    if(anyNA(clustervar)){ data$cluster.id <- 1:nrow(data) 
-    } else { data$cluster.id <- data[[clustervar]] }
-    cluster.set <- unique(data$cluster.id)
-  # Generate original target variable
-    point.estimate <- FUN(data)
-  # Create distribution of bootstrapped samples
-    estimates.bootstrapped <- replicate(iters,{
-      # Generate sample of clusters to include
-        clust.list <- sample(cluster.set,length(cluster.set),replace = TRUE)
-      # Build dataset from cluster list
-        data.clust <- do.call(rbind,lapply(1:length(clust.list), function(x) {
-          data[data$cluster.id == clust.list[x],]
-        }))
-      # Run function on new data
-        tryCatch(FUN(data.clust),finally=NA)
-    },
-    simplify=TRUE)
-  # Outcomes measures
-    if(is.matrix(estimates.bootstrapped)){
-      n_estimates <- nrow(estimates.bootstrapped)
-      SE <- unlist(lapply(1:n_estimates,function(x) {sd(estimates.bootstrapped[x,],na.rm = TRUE)}))
-      if (tails == "two-tailed"){
-        CI.lb <- unlist(lapply(1:n_estimates,function(x) {quantile(estimates.bootstrapped[x,], alpha/2,na.rm = TRUE)}))
-        CI.ub <- unlist(lapply(1:n_estimates,function(x) {quantile(estimates.bootstrapped[x,], 1-alpha/2,na.rm = TRUE)}))
-      } else if (tails == "one-tailed, upper"){
-        CI.lb <- unlist(lapply(1:n_estimates,function(x) {NA}))
-        CI.ub <- unlist(lapply(1:n_estimates,function(x) {quantile(estimates.bootstrapped[x,], 1-alpha,na.rm = TRUE)}))
-      } else if (tails == "one-tailed, lower"){
-        CI.lb <- unlist(lapply(1:n_estimates,function(x) {quantile(estimates.bootstrapped[x,], alpha,na.rm = TRUE)}))
-        CI.ub <- unlist(lapply(1:n_estimates,function(x) {NA}))
-      }
-    } else {
-      SE <- sd(estimates.bootstrapped,na.rm = TRUE)
-      if (tails == "two-tailed"){
-        CI.lb <- quantile(estimates.bootstrapped, alpha/2,na.rm = TRUE)
-        CI.ub <- quantile(estimates.bootstrapped, 1-alpha/2,na.rm = TRUE)
-      } else if (tails == "one-tailed, upper"){
-        CI.lb <- NA
-        CI.ub <- quantile(estimates.bootstrapped, 1-alpha,na.rm = TRUE)
-      } else if (tails == "one-tailed, lower"){
-        CI.lb <- quantile(estimates.bootstrapped, alpha,na.rm = TRUE)
-        CI.ub <- NA
-      }
-    }
+# Play area 
+if(FALSE){
+  # Main 3
+  {
+    df.hierarchy <- repli_export %>% group_by(paper_id,claim_id,rr_analytic_sample_stage,rr_is_manylabs, .drop = FALSE) %>% count()
+    dup_paper_ids <- df.hierarchy$paper_id[df.hierarchy$n==2]
+    repli_export_dups <- repli_export[repli_export$paper_id %in% dup_paper_ids,]
     
-  # Outputs
-    return(list("point.estimate"=point.estimate,"SE"=SE,
-                "CI.lb"=CI.lb,"CI.ub"=CI.ub,"estimates.bootstrapped"=estimates.bootstrapped))
+    test <- repli_export %>% group_by(paper_id,claim_id,rr_is_manylabs, .drop = FALSE) %>% count()
+    
+  }
+  
+  
+  
+  # Generate alluvial
+  {
+    df.repli.no.hier <- repli_export[c("paper_id","claim_id","rr_is_manylabs","rr_analytic_sample_stage","rr_id")]
+    df.repli.no.hier$claim_id <- paste0(df.repli.no.hier$paper_id,"_", df.repli.no.hier$claim_id)
+    
+    df.repli.no.hier <- df.repli.no.hier[df.repli.no.hier$paper_id %in% unique(df.repli.no.hier$paper_id)[60:120],]
+    
+    df <- df.repli.no.hier %>%
+      #make_long(paper_id, claim_id,rr_is_manylabs,rr_analytic_sample_stage)
+      make_long(paper_id, claim_id,rr_is_manylabs,rr_analytic_sample_stage)
+    
+    
+    pl <- ggplot(df, aes(x = x, next_x = next_x, node = node, next_node = next_node, fill = factor(node), label = node)) + 
+      geom_alluvial(flow.alpha = 0.75, show.legend = FALSE,space=1) +
+      #geom_sankey_label(size = 3, color = "black", fill= "white", hjust = -0.5)+
+      theme_bw() + 
+      theme(legend.position = "none") +
+      theme(axis.title = element_blank()
+            , axis.text.y = element_blank()
+            , axis.ticks = element_blank()  
+            , panel.grid = element_blank()) +
+      labs(fill = 'Nodes')
+     pl <- pl + scale_fill_viridis_d(option = "inferno")
+    # pl <- pl + labs(title = "Sankey diagram using ggplot")
+    # pl <- pl + labs(subtitle = "using  David Sjoberg's ggsankey package")
+    pl
+  }
 }
 
-test <- bootstrap.clust(data=repli_outcomes[c("paper_id","claim_id","repli_pattern_criteria_met")],FUN=function(data) {
-  mean(data$repli_pattern_criteria_met,na.rm=TRUE)
-},clustervar = "paper_id")
-test$point.estimate
 
-test <- bootstrap.clust(data=repli_outcomes[c("paper_id","claim_id","repli_pattern_criteria_met")],FUN=function(data) {
-  mean(data$repli_pattern_criteria_met,na.rm=TRUE)
-},clustervar = "paper_id")
-test$point.estimate
-
-test <- bootstrap.clust(data=repli_outcomes[c("paper_id","claim_id","repli_pattern_criteria_met","repli_type")],FUN=function(data) {
-  reg <- lm("repli_pattern_criteria_met ~ repli_type",data=data)
-  reg$coefficients
-},clustervar = "paper_id")
-test$point.estimate
-
-test <- lm("repli_pattern_criteria_met ~ repli_type",data=repli_outcomes)
-
-
-
-repli_outcomes$new_data <- repli_outcomes$repli_type=="new data"
-table(repli_outcomes$repli_pattern_criteria_met,repli_outcomes$new_data)
-
-rr <- (sum(repli_outcomes[repli_outcomes$repli_type=="new data",]$repli_pattern_criteria_met==TRUE)/sum(repli_outcomes$repli_type=="new data"))/
-(sum(repli_outcomes[repli_outcomes$repli_type=="secondary data",]$repli_pattern_criteria_met==TRUE)/sum(repli_outcomes$repli_type=="secondary data"))
-
-test$coefficients
