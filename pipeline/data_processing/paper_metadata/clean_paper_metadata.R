2e420e85268ec6cb4184f3698c8668a4d31e7932
Theresa Wisneskie Stankov <82187440+twisneskie@users.noreply.github.com>
2024-02-21 15:48:13 -0500

    combined publication categories

--- pipeline/data_processing/paper_metadata/clean_paper_metadata.R
+++ pipeline/data_processing/paper_metadata/clean_paper_metadata.R
@@ -14,6 +14,17 @@ merge_paper_metadata <- function(all_metadata_filled,
               publication_WOS)) %>%
     rename(ISSN = ISSN_CR,
            pub_year = pub_year_CR) %>%
-    left_join(publications, by = "ISSN")
+    left_join(publications, by = "ISSN") %>%
+    mutate(
+      COS_pub_category = case_match(
+        COS_pub_category,
+        c("marketing/org behavior", "management") ~ "business",
+        "economics" ~ "economics and finance",
+        c("criminology", "sociology") ~ "sociology and criminology",
+        "public administration" ~ "political science",
+        c("health", "psychology") ~ "psychology and health",
+        .default = COS_pub_category
+      )
+    )
 
 }
