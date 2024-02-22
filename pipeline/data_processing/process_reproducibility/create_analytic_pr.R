4af14c5a28273c9ea1e2249204d6cc023b40a00a
Theresa Wisneskie Stankov <82187440+twisneskie@users.noreply.github.com>
2024-02-22 13:38:34 -0500

    Add PR outcomes analytic dataset

--- pipeline/data_processing/process_reproducibility/create_analytic_pr.R
+++ pipeline/data_processing/process_reproducibility/create_analytic_pr.R
@@ -1,16 +1,14 @@
-create_pr_analytic <- function(pr_data_form) {
+create_pr_analytic <- function(pr_qc,
+                               oa_outreach) {
   
-  pr_outcomes <- pr_data_form %>%
-    rename(
-      paper_data_type = existing_data,
-      paper_link = paper_data_link,
-      paper_link_content = paper_data_link_content,
-      paper_instructions = paper_data_instructions,
-      paper_instructions_content = paper_data_instructions_content,
-      paper_request = paper_materials_available_on_request,
-      paper_request_content = paper_materials_available_on_request_content,
-      pr_notes = other_notes
-    ) %>%
+  oa_fields <- oa_outreach %>%
+    select(OA_data_shared,
+           OA_code_shared,
+           OA_source_data_shared,
+           paper_id)
+  
+  pr_qc %>%
+    left_join(oa_fields, by = "paper_id") %>%
     mutate(
       pr_data_location = case_match(
         pr_data_location,
@@ -48,10 +46,57 @@ create_pr_analytic <- function(pr_data_form) {
         pr_code_complete,
         "Partial/Incomplete" ~ "Incomplete",
         .default = pr_code_complete
+      ),
+      public_source_data_and_code = "No",
+      process_reproducible = case_when(
+        pr_data_available == "Yes" & pr_code_available == "Yes" ~ "Yes",
+        public_source_data_and_code == "Yes" ~ "Yes",
+        .default = "No"
+      ),
+      OA_data_shared = case_when(
+        OA_data_shared == "available" ~ "available_online",
+        pr_data_available == "Yes" ~ "available_online",
+        is.na(OA_data_shared) ~ "no",
+        .default = OA_data_shared
+      ),
+      OA_code_shared = case_when(
+        OA_code_shared == "available" ~ "available_online",
+        pr_code_available == "Yes" ~ "available_online",
+        is.na(OA_code_shared) ~ "no",
+        .default = OA_code_shared
       )
     ) %>%
-    select(-c(paper_data_link_functioning,
-              pr_code_responses_same_as_data,
-              is_vor))
+    select(pdf_filename,
+           paper_id,
+           covid,
+           paper_data_type = existing_data,
+           paper_link = paper_data_link,
+           paper_link_content = paper_data_link_content,
+           paper_instructions = paper_data_instructions,
+           paper_instructions_content = paper_data_instructions_content,
+           paper_request = paper_materials_available_on_request,
+           paper_request_content = paper_materials_available_on_request_content,
+           data_available = pr_data_available,
+           data_location = pr_data_location,
+           data_location_description = pr_data_location_description,
+           data_complete = pr_data_complete,
+           data_date = pr_data_date,
+           data_notes = pr_data_notes,
+           restricted_data,
+           restricted_data_instructions,
+           restricted_data_description,
+           code_available = pr_code_available,
+           restricted_data_category,
+           code_location = pr_code_location,
+           code_location_description = pr_code_location_description,
+           code_complete = pr_code_complete,
+           code_date = pr_code_date,
+           code_notes = pr_code_notes,
+           pr_notes = other_notes,
+           public_source_data_and_code,
+           process_reproducible,
+           OA_data_shared,
+           OA_code_shared,
+           OA_source_data_shared)
   
 }
