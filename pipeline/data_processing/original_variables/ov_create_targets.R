b0b4af473a3dfad286dd5e02934dd5d0d353e957
Theresa Wisneskie Stankov <82187440+twisneskie@users.noreply.github.com>
2024-07-08 11:52:50 -0400

    overhaul ov pipeline - streamlined and made steps more logical

--- pipeline/data_processing/original_variables/ov_create_targets.R
+++ pipeline/data_processing/original_variables/ov_create_targets.R
@@ -1,7 +1,78 @@
-# Create Original Variable Analytic Dataset
+# Create original variable targets
 
+# Transform, merge, and update raw data
+export_orig <- function(orig_statistics_output_p2,
+                        orig_statistics_dataset_p1,
+                        original_inftest_dataset,
+                        orig_statistics_manual_data_entry,
+                        orig_vars_qa,
+                        tagtable_covid_p1,
+                        orig_input_changelog) {
+
+  orig_extended <- transform_orig_output(orig_statistics_output_p2)
+
+  merge_orig_input(orig_statistics_dataset_p1,
+                   original_inftest_dataset,
+                   orig_statistics_manual_data_entry,
+                   orig_vars_qa,
+                   tagtable_covid_p1) %>%
+    update_orig_input(orig_input_changelog) %>%
+    mutate(
+      # Prioritize which version of duplicates to keep
+      data_source_priority = case_match(original_data_source,
+                                        "p2_manual" ~ 1,
+                                        "p2_power" ~ 2,
+                                        "p2_pipeline" ~ 3,
+                                        "orig_via_repro_form" ~ 4,
+                                        "pipeline_orig" ~ 5,
+                                        "repro" ~ 6),
+      # Standardize values for original_effect_size_type_reported and
+      # original_effect_size_type_reference
+      across(
+        c(original_effect_size_type_reported,
+          original_effect_size_type_reference),
+        ~ case_match(
+          .x,
+          c("Cohen's d", "d", "d_sample", "d_sample (one)") ~ "cohen_d",
+          "Cohen's dz" ~ "cohen_dz",
+          c("Cohen's f-squared", "cohenf2") ~ "cohen_f_squared",
+          c("Cohen's w", "cohens_w") ~ "cohen_w",
+          "Cramer's V" ~ "cramer_v",
+          "Eta-squared" ~ "eta_squared",
+          "Hazard ratio" ~ "hazard_ratio",
+          c("adjusted_odds_ratio", "Odds ratio") ~ "odds_ratio",
+          "Partial eta-squared" ~ "partial_eta_squared",
+          "path_coef" ~ "path_coefficient",
+          c("Pearson's r", "r") ~ "pearson_r",
+          "Phi" ~ "phi",
+          "probit_coefficient_estimate" ~ "probit_coefficient",
+          c("multiple_regression_coefficient",
+            "raw_ols_coefficient",
+            "raw_regression_coefficient",
+            "raw_regression_coefficient_from_a_two_step_system_gmm",
+            "raw_regression_coefficient_beta") ~ "regression_coefficient",
+          "relative risk ratio" ~ "relative_risk_ratio",
+          c("SER method", "SER-Method") ~ "ser_method",
+          "SER-t method" ~ "ser_method_t",
+          c("standardized_regression_coefficient",
+            "standardized_coefficient") ~ "standardized_regression_coefficient",
+          .default = .x
+        )
+      )
+    ) %>%
+    arrange(unique_claim_id, data_source_priority) %>%
+    relocate(data_source_priority, .after = original_data_source) %>%
+    distinct(unique_claim_id, .keep_all = TRUE) %>%
+    select(-c(data_source_priority)) %>%
+    left_join(orig_extended, join_by(paper_id, claim_id))
+
+}
+
+# Create Original Variable Analytic Dataset
 create_ov_analytic <- function(orig_dataset,
                                complex_bushel) {
+  
+  bushel <- transform_complex_bushel(complex_bushel)
 
   orig_dataset %>%
     mutate(claim_id = unique_claim_id) %>%
@@ -41,10 +112,10 @@ create_ov_analytic <- function(orig_dataset,
       orig_power_for_50_effect = original_power_50_original_effect,
       orig_power_for_75_effect = original_power_75_original_effect
     ) %>%
-    # Indicator for if the evidence for a bushel claim deviates from the 
-    # prototypical SCORE evidence format (a single, statistically significant 
+    # Indicator for if the evidence for a bushel claim deviates from the
+    # prototypical SCORE evidence format (a single, statistically significant
     # test result)
-    left_join(complex_bushel, by = "unique_claim_id") %>%
+    left_join(bushel, by = "unique_claim_id") %>%
     # Kill unneeded variables
     select(-c(unique_claim_id,
               original_data_source,
---
+++ pipeline/data_processing/original_variables/ov_transform.R
@@ -0,0 +1,162 @@
+# Original Variable Transform Functions
+
+# Transform Tilburg extension data
+transform_orig_output <- function(orig_statistics_output_p2) {
+
+  orig_statistics_output_p2 %>%
+    # All character values and variable names are encased in quotes, so we need
+    # to deal with that to get all of the lines to read in correctly, even when
+    # there is quoted text within a value. Values that contain actual
+    # quotations will have double quotes replaced with single quotes.
+    mutate(
+      across(where(is.character), ~ str_remove_all(.x, "\"")),
+      across(where(is.character), ~ str_replace_all(.x, "\\\\", "\'"))
+    ) %>%
+    rename_with(~ str_remove_all(.x, "\"")) %>%
+    select(paper_id,
+           claim_id = c4_claim_id,
+           original_statistic_analysis_type_statsteam = analysis_type,
+           original_pearsons_r_defined,
+           original_pearsons_r_numeric,
+           original_es_lb_ci_pearson,
+           original_es_ub_ci_pearson,
+           original_effect_size_type_statsteam,
+           original_effect_size_value_statsteam,
+           original_es_lb_ci_nativeunits,
+           original_es_ub_ci_nativeunits,
+           original_power_small,
+           original_power_medium,
+           original_power_50_original_effect,
+           original_power_75_original_effect,
+           Tilburg_team_finished) %>%
+    # Including "skipped" as finished because Tilburg has finished what they
+    # can/will do with that claim
+    mutate(Tilburg_team_finished = case_match(Tilburg_team_finished,
+                                              "skipped" ~ TRUE,
+                                              "finished" ~ TRUE,
+                                              NA ~ NA,
+                                              .default = FALSE))
+
+}
+
+# Transform P1 original variables data
+transform_orig_p1 <- function(orig_statistics_dataset_p1) {
+
+  orig_statistics_dataset_p1 %>%
+    # Where there are duplicates, prefer version coded orig_via_repro_form
+    # under the assumption it was an update to a previous entry
+    arrange(paper_id, original_data_source) %>%
+    distinct(paper_id, .keep_all = TRUE) %>%
+    mutate(
+      # Records coded orig_via_repro_form have some unexpected new fields that
+      # only contain values for such cases and seem to correspond to
+      # canonical variables - put those values where they seem to belong.
+      # Of note, original_p_value_type_reference and
+      # original_effect_size_fulltext_reference also fall into this category
+      # of being unique to orig_via_repro_form cases. However, they are valid
+      # columns in phase 2 original variables coding, so I do not move their
+      # values to analogous fields during this step.
+      original_analysis_type_COS = coalesce(
+        original_analysis_type_COS,
+        original_statistic_analysis_type_reference
+      ),
+      original_statistic_effect_type_reported = coalesce(
+        original_statistic_effect_type_reported,
+        original_statistic_interaction_reported
+      ),
+      original_statistic_effect_type_reference = coalesce(
+        original_statistic_effect_type_reference,
+        original_statistic_interaction_reference
+      ),
+      claim_id = "single-trace"
+    ) %>%
+    rename(
+      original_statistic_analysis_type = original_analysis_type_COS,
+      original_samplesize_calculation_contributor = original_cos_contributor
+    )
+
+}
+
+# Transform original variables inftest data
+transform_orig_inftest <- function(original_inftest_dataset) {
+
+  original_inftest_dataset %>%
+    mutate(
+      claim_id = "single-trace",
+      original_data_source = "p2_power",
+      # Sometimes analysis type was set under a different variable name.
+      # If that is missing in the expected field
+      # (original_statistic_analysis_type), try to fill it in from
+      # original_statistic_analysis_cos
+      original_statistic_analysis_type = coalesce(
+        original_statistic_analysis_type,
+        original_analysis_type_cos
+      ),
+      # Sometimes sample size units are identified under a made up variable -
+      # original_analytic_sample_size_units_reference - but
+      # original_analytic_sample_size_units_reported is the canonical original
+      # sample size units field, so bring those values over where they are
+      # missing in the expected field
+      original_analytic_sample_size_units_reported = coalesce(
+        original_analytic_sample_size_units_reported,
+        original_analytic_sample_size_units_reference
+      )
+    ) %>%
+    # These variables were misnamed during this coding pipeline and need to
+    # be standardized to merge with other sources of original variables
+    rename(
+      original_poweranalysis_link = rr_poweranalysis_link,
+      original_statistic_effect_type_reported =
+        original_statistic_interaction_reported,
+      original_statistic_effect_type_reference =
+        original_statistic_interaction_reference
+    )
+
+}
+
+# Transform original variables manual data
+transform_orig_manual <- function(orig_statistics_manual_data_entry) {
+
+  orig_statistics_manual_data_entry %>%
+    mutate(
+      original_data_source = "p2_manual",
+      # sometimes sample size units are identified under a made up variable -
+      # original_analytic_sample_size_units_reference - but
+      # original_analytic_sample_size_units_reported is the canonical original
+      # sample size units field, so bring those values over where they are
+      # missing in the expected field
+      original_analytic_sample_size_units_reported = coalesce(
+        original_analytic_sample_size_units_reported,
+        original_analytic_sample_size_units_reference
+      )
+    ) %>%
+    # These variables were misnamed during this coding pipeline and need to
+    # be standardized to merge with other sources of original variables
+    rename(
+      claim_id = c4_claim_id,
+      original_statistic_analysis_type = original_analysis_type_cos,
+      original_poweranalysis_link = rr_poweranalysis_link,
+      original_statistic_effect_type_reported =
+        original_statistic_interaction_reported,
+      original_statistic_effect_type_reference =
+        original_statistic_interaction_reference
+    )
+}
+
+# Transform original variables entered through the Google form
+transform_orig_qa <- function(orig_vars_qa) {
+
+  orig_vars_qa %>%
+    mutate(original_data_source = "p2_pipeline") %>%
+    rename(claim_id = c4_claim_id)
+
+}
+
+# Transform complex bushel
+transform_complex_bushel <- function(complex_bushel) {
+  
+  complex_bushel %>%
+  mutate(unique_claim_id = str_c(paper_id, "_", claim_id)) %>%
+        select(unique_claim_id,
+               bushel_complex = complex)
+}
