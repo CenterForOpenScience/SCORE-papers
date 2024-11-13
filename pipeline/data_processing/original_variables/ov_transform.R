0f47167d948b34b4c3626e012ed74b6a02e24983
Theresa Wisneskie Stankov <82187440+twisneskie@users.noreply.github.com>
2024-11-13 14:43:44 -0500

    change bushel_complex to boolean

--- pipeline/data_processing/original_variables/ov_transform.R
+++ pipeline/data_processing/original_variables/ov_transform.R
@@ -156,7 +156,8 @@ transform_orig_qa <- function(orig_vars_qa) {
 transform_complex_bushel <- function(complex_bushel) {
   
   complex_bushel %>%
-  mutate(unique_claim_id = str_c(paper_id, "_", claim_id)) %>%
+  mutate(unique_claim_id = str_c(paper_id, "_", claim_id),
+         complex = as.logical(complex)) %>%
         select(unique_claim_id,
                bushel_complex = complex)
 }
