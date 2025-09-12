bea397daf1d6af41f7c99354de681c7d270facbd
Theresa Wisneskie Stankov <82187440+twisneskie@users.noreply.github.com>
2025-09-12 10:49:28 -0400

    add citation info for papers

--- pipeline/data_processing/paper_metadata/merge_paper_metadata.R
+++ pipeline/data_processing/paper_metadata/merge_paper_metadata.R
@@ -1,11 +1,13 @@
 merge_paper_metadata <- function(non_covid_metadata,
                                  covid_metadata,
+                                 paper_cite,
                                  publications) {
   
   non_covid_metadata %>%
     left_join(publications, by = "ISSN") %>%
     add_row(covid_metadata) %>%
     # Standardize cases
-    mutate(across(c(title, author_last, author_first), str_to_title))
+    mutate(across(c(title, author_last, author_first), str_to_title)) %>%
+    left_join(paper_cite %>% select(paper_id, citation), by = "paper_id")
   
 }
